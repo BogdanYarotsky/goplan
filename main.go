95f3220edf775e037707687d911101e0a9490441
BogdanYarotsky <byarotsky@gmail.com>
2023-12-16 21:51:33 +0100

    wip - server refactorings

--- main.go
+++ main.go
@@ -6,37 +6,57 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 )
+
+type TimeSlot struct {
+	id        int
+	machineId int
+	contentId int
+	start     time.Time
+	end       time.Time
+}
+
+var slots = []*TimeSlot{
+	{
+		1, 1, 1, time.Now(), time.Now(),
+	},
+}
 
 type Handler struct {
 	l *log.Logger
 }
 
 func (s *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println("hello")
 }
 
 func main() {
-	l := log.New(os.Stdout, "planning-api", log.LstdFlags)
+	l := log.New(os.Stdout, "plan-api", log.LstdFlags)
 	sm := http.NewServeMux()
 	h := &Handler{l}
 	sm.Handle("/", h)
-
 	s := &http.Server{
 		Addr:    "localhost:9090",
 		Handler: sm,
 	}
 
 	go func() {
-		s.ListenAndServe()
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
 	}()
 
-	sigChannel := make(chan os.Signal, 2)
-	signal.Notify(sigChannel, os.Interrupt)
+	sigChannel := make(chan os.Signal, 1)
+	signals := []os.Signal{os.Interrupt, syscall.SIGTERM}
+	signal.Notify(sigChannel, signals...)
 	sig := <-sigChannel
 	l.Printf("Received %s signal, shutting down\n", sig)
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	s.Shutdown(ctx)
-	cancel()
+	defer cancel()
+	err := s.Shutdown(ctx)
+	if err != nil {
+		l.Fatal(err)
+	}
 }
